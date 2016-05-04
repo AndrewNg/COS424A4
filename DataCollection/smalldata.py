767963bf57513cf756e2098fe1b5f2a05fcf25d5
Andrew Ng <ajng21@gmail.com>
2016-05-04 00:57:40 -0400

    small fix, data is now html parsed

--- DataCollection/smalldata.py
+++ DataCollection/smalldata.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from html.parser import HTMLParser
 
 data = pd.read_csv("../data/data.csv")
+parser = HTMLParser()
 
 data = data.loc[data['link'].str.contains('http://reddit.com/r/aww')]
 data = data['body']
@@ -10,7 +12,7 @@ print(data.head(n=10))
 file = open('smalldata.txt', 'w')
 
 for comment in data:
-  file.write(comment)
+  file.write(parser.unescape(comment))
   file.write('@`@')
 
 file.close()
