b88c6136edef5ae40f9c657dd98e9cf25ab6bd9f
Andrew Ng <ajng21@gmail.com>
2016-05-09 05:04:32 -0400

    new code for sworkbook

--- Clustering/ascii.py
+++ Clustering/ascii.py
@@ -1,4 +1,14 @@
-nonascii = bytearray(range(0x80, 0x100))
-with open('../data/cluster9.txt','rb') as infile, open('../data/cluster9_parsed.txt','wb') as outfile:
-    for line in infile: # b'\n'-separated lines (Linux, OSX, Windows)
-        outfile.write(line.translate(None, nonascii))
+# nonascii = bytearray(range(0x80, 0x100))
+# with open('../data/cluster9.txt','rb') as infile, open('../data/cluster9_parsed.txt','wb') as outfile:
+#     for line in infile: # b'\n'-separated lines (Linux, OSX, Windows)
+#         outfile.write(line.translate(None, nonascii))
+
+import pandas as pd
+
+data = pd.read_csv('../data/finalnbanostop.csv')
+
+for index, row in data.iterrows():
+  row['comment'] = str(row['comment']).encode('ascii',errors='ignore')
+  row['title'] = str(row['title']).encode('ascii',errors='ignore')
+
+data.to_csv('../data/finalnbanostoponlyascii.csv')
